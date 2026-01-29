176307d7fefe8a901db3864f7c9ab9961bfb684e
Phoenix255DD <2330469@upv.edu.mx>
2026-01-29 00:58:34 -0600

    Commit 2

---
+++ wordpress/wp-content/plugins/ninja-forms/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'saturday-drive/ninja-forms',
+        'pretty_version' => 'dev-e236c3cd7af8a264e1b17155c7a46434d4ebbfd6',
+        'version' => 'dev-e236c3cd7af8a264e1b17155c7a46434d4ebbfd6',
+        'reference' => 'e236c3cd7af8a264e1b17155c7a46434d4ebbfd6',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'saturday-drive/ninja-forms' => array(
+            'pretty_version' => 'dev-e236c3cd7af8a264e1b17155c7a46434d4ebbfd6',
+            'version' => 'dev-e236c3cd7af8a264e1b17155c7a46434d4ebbfd6',
+            'reference' => 'e236c3cd7af8a264e1b17155c7a46434d4ebbfd6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
