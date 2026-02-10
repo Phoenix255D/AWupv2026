268ba9effc9c3a22f52e39e4a260b6111bb67375
Phoenix255DD <2330469@upv.edu.mx>
2026-02-10 07:46:44 -0600

    commit p

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -23,10 +23,10 @@
 define( 'DB_NAME', 'wordpressdb' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'admin' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'aa23bc747d86edd99fe548a6d5644d785178a80b981ece64' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
